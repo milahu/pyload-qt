e45e53ca83f96fa34f78b1ddc778930d07b96db5
Milan Hauth <milahu@milahu.duckdns.org>
2026-03-27 07:05:30 +0100

    fix shell.nix for qt 6.5.0

--- shell.nix
+++ shell.nix
@@ -9,4 +9,9 @@ pkgs.mkShell {
       black
     ]))
   ];
+  # fix: qt.qpa.plugin: From 6.5.0, xcb-cursor0 or libxcb-cursor0 is needed to load the Qt xcb platform plugin.
+  # https://github.com/NixOS/nixpkgs/issues/431908
+  shellHook = ''
+    export QT_PLUGIN_PATH=${with pkgs.libsForQt5; "${qtbase}/${qtbase.qtPluginPrefix}"}
+  '';
 }
